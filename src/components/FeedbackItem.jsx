50986128e937ea4fdc8aa346a6599c1ad1df13c2
Illia Ostrovskyi <illyaostrovsky.99@gmail.com>
2024-03-09 22:47:47 +0200

    feat: repakce render with createRoot, update proprTypes validation, create Card component

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
 
-function FeedbackItem({rating, text}) {
+function FeedbackItem({item}) {
     return (
-        <div className='card'>
-          <div className="num-display">{rating}</div>
-          <div className="text-display">{text}</div>
-        </div> 
+        <Card rev>
+          <div className="num-display">{item.rating}</div>
+          <div className="text-display">{item.text}</div>
+        </Card> 
     )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
